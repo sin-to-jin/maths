28dca44a2db1f1229069042e0003bf193e1a3037
sin <ogasawaraShinnosuke@users.noreply.github.com>
2019-03-21 04:11:54 +0900

    Update goldbachs_conjecture_process.rb

--- src/goldbachs_conjecture_process.rb
+++ src/goldbachs_conjecture_process.rb
@@ -1,43 +1,13 @@
 #!/usr/bin/ruby
-class PrimeNumbers
-  attr_reader :max, :half, :primes
-  def initialize(max)
-    @max = max
-    @primes = []
-    build
-  end
 
-  def build
-    [*1..max].each do |i|
-      next if i < 2 || i.even?
-      primes << i if prime?(i)
-    end
-  end
-
-  private
-
-  def prime?(x)
-    [*2..(x-1)].each { |i| return false if (x % i.to_f).zero? }
-  end
-end
-
-class Formula
-  attr_accessor :answer, :left, :right
-  def initialize(a, m)
-    @answer = a
-    @left = a - m
-    @right = m
-  end
-
-  def vals
-    "#{answer} = #{left} + #{right}"
-  end
-end
+require_relative './lib/prime_numbers'
 
 class GoldbachsConjecture
+  include PrimeNumbers
   attr_accessor :formulas
+
   def initialize(max)
-    @pn = PrimeNumbers.new(max)
+    @pn = Values.new(max)
     formula!
     build!
   end
