9268530193ee8a407bc5421aca969987e82fcd63
sin <ogasawaraShinnosuke@users.noreply.github.com>
2019-03-21 15:53:02 +0900

    refactoring.

--- src/goldbachs_conjecture.rb
+++ src/goldbachs_conjecture.rb
@@ -9,17 +9,28 @@ class GoldbachsConjecture
   def initialize(max)
     @pn = Values.new(max)
     formula!
-    build!
-  end
-
-  def formula!
-    @formulas = [*1..pn.max].each_with_object([]) { |i, f| pn.primes.each { |n| f << Formula.new(i, n) } if i > 2 }
   end
 
   private
   attr_reader :pn
 
-  def build!
-    @formulas.select! { |v| v.answer.even? && v.right <= v.answer && (v.answer == 4 || v.right <= v.answer / 2) }
+  def formula!
+    @formulas = [*1..pn.max].each_with_object([]) do |i, f|
+      pn.primes.each { |n| f << GoldbachsFormula.new(pn, i, n) } if i > 2
+    end.select!(&:goldbachs)
+  end
+
+  class GoldbachsFormula < Formula
+    attr_accessor :pn
+
+    def initialize(pn, a, m)
+      @pn = pn
+      super(a, m)
+    end
+
+    def goldbachs
+      even_uniq! && pn.includes?([left, right])
+    end
   end
 end
+
--- src/lib/prime_numbers.rb
+++ src/lib/prime_numbers.rb
@@ -2,21 +2,25 @@
 
 module PrimeNumbers
   class Values
-    attr_reader :max, :half, :primes
+    attr_reader :max, :primes
     def initialize(max)
       @max = max
-      @primes = []
-      build
+      primes!
     end
 
-    def build
-      [*1..max].each do |i|
-        next if i < 2 || i.even?
-        primes << i if prime?(i)
-      end
+    def includes?(array)
+      (primes & array).length == array.uniq.length
     end
 
     private
+
+    def primes!
+      @primes ||= []
+      [*1..max].each do |i|
+        next if i < 2 || i.even?
+        @primes << i if prime?(i)
+      end if max > 1
+    end
 
     def prime?(x)
       [*2..(x-1)].each { |i| return false if (x % i.to_f).zero? }
@@ -31,8 +35,13 @@ module PrimeNumbers
       @right = m
     end
 
-    def vals
+    def to_s
       "#{answer} = #{left} + #{right}"
+    end
+
+    def even_uniq!
+      answer.even? && right <= answer && right <= answer / 2
     end
   end
 end
+
