5b362bd17c222f796cdd59753e7ff24ee22dbae0
sin <ogasawaraShinnosuke@users.noreply.github.com>
2020-07-24 12:21:41 +0900

    Update financial_utils.rb

--- src/lib/financial_utils.rb
+++ src/lib/financial_utils.rb
@@ -1,4 +1,10 @@
 module FinancialUtils
+  # Get security password for LifeCard.
+  #
+  # @param fullname CreditCard Fullname
+  # @param first letter index
+  # @param second letter index
+  # @return hash[:first] => first password, hash[:second] => second password 
   def sec_auth_life_card(fullname, password1, password2)
     master = fullname.chars.each.with_index(1).to_h { |v, i| [i, v] }
     { first: master[password1], second: master[password2] }
